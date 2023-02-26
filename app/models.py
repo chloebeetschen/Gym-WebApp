8abdb8e4a6e9abbb508ca89986eef91c87e55d30
toby hutchinson <tobyhutchinson@gmail.com>
2023-02-26 16:15:19 +0000

    User's can now log in with their email and password

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
+from flask_login import UserMixin
 
 
 # Login details
-class UserLogin(db.Model):
+class UserLogin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  # userId
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)  # Encrypt (Hash)
@@ -11,7 +12,7 @@ class UserLogin(db.Model):
 
 
 # User info (Sensitive info -> encryption)
-class UserDetails(db.Model):
+class UserDetails(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  # userDetailId
     name = db.Column(db.String(150), nullable=False)
     dateOfBirth = db.Column(db.Date, nullable=False)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, flash, url_for, redirect
 from app import app, db, models
 from .forms import *
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt(app)
 
+loginManager = LoginManager()
+loginManager.init_app(app)
+loginManager.login_view = "Login"
+
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+@loginManager.user_loader
+def loadUser(userId):
+    return models.UserLogin.query.get(int(userId))
 
 @app.route('/')
 def index():
@@ -16,8 +25,24 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = models.UserLogin.query.filter_by(email=form.Email.data).first()
+        
+        if user:
+            # Check the password hash against the stored hashed password
+            if bcrypt.check_password_hash(user.password, form.Password.data):
+                login_user(user)
+                return redirect(url_for('home'))
+
     return render_template('login.html',
                             form = form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -26,7 +51,6 @@ def register():
     if form.validate_on_submit():
         # Check that the email hasn't been used already.
         usedEmail = models.UserLogin.query.filter_by(email=form.Email.data).first()
-        print(1)
         if usedEmail:
             flash("Looks like this email is already in use. Please log in.")
             return redirect(url_for('login'))
@@ -41,13 +65,18 @@ def register():
         # Create new user and details
         newUser = models.UserLogin(email=Email, password=hashedPassword)
         newUserDetails = models.UserDetails(name=Name, dateOfBirth=dob, address=Address, loginDetails=newUser.id)
-        print(2)
 
         # Add to the database
         db.session.add(newUser)
         db.session.add(newUserDetails)
         db.session.commit()
-        print(3)
+        return redirect(url_for('login'))
 
     return render_template('register.html',
                             form = form)
+
+@app.route('/home', methods=['GET', 'POST'])
+@login_required # You have to be logged in to see the homepage
+def home():
+
+    return render_template('home.html')

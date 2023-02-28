a9f19b348dd884bdfe680ffa4fa4f2111e173ade
Chloe Beetschen <sc21cab@leeds.ac.uk>
2023-02-28 21:13:19 +0000

    forms implemented

--- app/forms.py
+++ app/forms.py
@@ -15,7 +15,6 @@ class addActivityForm(FlaskForm):
 
 #for the manager to edit an activity e.g. swimming
 class editActivityForm(FlaskForm):
-    aType = StringField('aType')
     aPrice = IntegerField('aPrice')
     aLocation = StringField('aLocation')
     aCapacity = IntegerField('aCapacity')
@@ -47,8 +46,6 @@ class editEventForm(FlaskForm):
     cTime = SelectField('cTime', choices = [(700,'07:00'), (730,'07:30'), (800,'08:00'), (830,'08:30'), (900,'09:00'), (900,'09:30'), (1000,'10:00'), (1030,'10:30'), (1100,'11:00'), (1130,'11:30'), (1200,'12:00'), (1230,'12:30'), (1300,'13:00'), (1330,'13:30'), (1400,'14:00'), (1430,'14:30'), (1500,'15:00'), (1530,'15:30'), (1600,'16:00'), (1630,'16:30'), (1700,'17:00'), (1730,'17:30'), (1800,'18:00'), (1830,'18:30'), (1900,'19:00'), (1930,'19:30'), (2000,'20:00'), (2030,'20:30'), (2100,'21:00'), (2130,'21:30')])
 
     cDuration = SelectField('cDuration', choices = [(30, '30mins'), (60, '60mins')])
-    cFull = BooleanField('cFull')
-    cCurrent = IntegerField('cCurrent')
 
     editEvent = SubmitField('editEvent')
 

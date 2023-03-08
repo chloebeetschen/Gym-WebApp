56298f97f92dc01f4a606a250d905415e08be8de
Archie Gerry <sc21ag@leeds.ac.uk>
2023-03-08 18:38:20 +0000

    Amended DB

--- app/models.py
+++ app/models.py
@@ -1,40 +1,39 @@
 from app import db
 from flask_login import UserMixin
 
+
+
 class Calendar(db.Model):
 
-    __tablename__ = "calendar"
 
     id = db.Column(db.Integer, primary_key=True)
 
     activityDate = db.Column(db.Date, nullable=False)
     activityTime = db.Column(db.Integer, nullable=False)
-    activityDuration = db.Column(db.Integer, nullable=False)
-    activityFull = db.Column(db.Boolean, nullable=False)
+    activityDuration = db.Column(db.Integer, nullable=False)    
+    activityStaffName = db.Column(db.String(250))
+    activityPrice = db.Column(db.Float)
+    activityCapacity = db.Column(db.Integer)
     #this is the number of people signed up to the activity, which will need to be incremented
-    activityCurrent = db.Column(db.Integer)
+    activitySlotsTaken = db.Column(db.Integer)
     
     # foreign key for activity table
-    #activityId = db.Column(db.Integer, db.ForeignKey('Activity.id'))
-    activityId = db.Column(db.Integer)
+    activityId = db.Column(db.Integer, db.ForeignKey('activity.id'))
+    #activityId = db.Column(db.Integer)
 
     # link to user bookings table
     #bookings = db.relationship('UserBookings', backref='calendar_user')\
 
 class Activity(db.Model):
 
-    __tablename__ = 'activity'
 
     id = db.Column(db.Integer, primary_key=True)
-
     activityType = db.Column(db.String(250), unique=True)
-    activityPrice = db.Column(db.Float)
-    activityLocation = db.Column(db.String(250))
-    activityCapacity = db.Column(db.Integer)
-    activityStaffName = db.Column(db.String(250))
+    calender = db.relationship('calender', backref='calender')
 
     #relationship with calendar
     #events = db.relationship('Calendar', backref='activity_info')
+
 
 class UserBookings(db.Model):
 

c3985fa3e0b5d70f1438ae0e845929383b24c006
Aaditi Agrawal [sc21aa] <sc21aa@comp-pc-049791.leeds.ac.uk>
2023-03-15 18:17:53 +0000

    added basket page testing and finished register user test

--- unit_testing.py
+++ unit_testing.py
@@ -4,6 +4,7 @@
 
 import unittest
 from flask import Flask
+from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 from app import app, db, models 
 from app.models import *
@@ -21,22 +22,16 @@ class TestCase(unittest.TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
 
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        
         self.app = app.test_client()
         db.create_all()
 
        
-
     #this deletes the test database once testing is complete 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    
-    #testing the navbar - checking that correct page is loaded for its corresponding button
-    #for al users
-    def test_navBarAll_login(self):
-        response = self.app.get(('/login'), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        #with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
     def test_navBarAll_register(self):
         response = self.app.get(('/register'), follow_redirects=True)
@@ -64,6 +59,9 @@ class TestCase(unittest.TestCase):
         response = self.app.get(('/settings'), follow_redirects = True)
         self.assertEqual(response.status_code, 200)
 
+    def test_navBarType1_basket(self):
+        response = self.app.get(('/basket'), follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
     #TO DO :
     #basket / payment page 
     #pricing list page 
@@ -75,11 +73,7 @@ class TestCase(unittest.TestCase):
 
     def test_navBarType2_addActivity(self):
         response = self.app.get(('/addActivity'), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)  
-
-    def test_navBarType2_viewAEManager(self):
-        response = self.app.get(('/viewAEManager'), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)  
+        self.assertEqual(response.status_code, 200)   
 
     def test_navBarType2_manageUsers(self):
         response = self.app.get(('/manageUsers'), follow_redirects=True)
@@ -88,28 +82,37 @@ class TestCase(unittest.TestCase):
 
     #registering a user and testing if this updates in the database
     def test_register(self):
-        data = {
-            'Name' : 'Michael Scott',
-            'DateOfBirth' : '1/10/1999',
-            'Address' : '7 Uni Road',
-            'Email' : ' michael.scott@gmail.com',
-            'Password' : '12345678',
-            'ReenterPassword' : '12345678'
-        }
-
-        form = RegisterForm(data=data)
-        self.assertTrue(form.validate())
-        response = self.app.post('/register', data=data)
+        response = self.app.get(('/register'), follow_redirects = True)
         self.assertEqual(response.status_code, 200)
 
-        user = UserLogin.query.filter_by(name= 'Michael Scott').first()
-        self.assertIsNotNone(user)
+        with app.test_request_context():
+            with app.app_context():
+                data = {
+                    'Name' : 'Michael Scott',
+                    'DateOfBirth' : datetime.strptime('2002-03-15', '%Y-%m-%d').date(),
+                    'Address' : '7 Uni Road',
+                    'Email' : ' michael.scott@gmail.com',
+                    'Password' : '12345678',
+                    'ReenterPassword' : '12345678',
+                    'Type' : 1
+                    }
+
+                form = RegisterForm(data=data)
+                self.assertTrue(form.validate())
+                if not form.validate():
+                    print(form.errors)
+
+                response = self.app.post('/register', data=data)
+                
+                user = models.UserDetails.query.filter_by(name= 'Michael Scott').first()
+                #user = models.UserLogin.query.filter_by(email= 'michael.scott@gmail.com').first()
+                self.assertIsNotNone(user)
 
 
 
     #testing that a registered user can log in 
     def test_login(self):
-        self.user1 = UserLogin(email = 'michael.scott@gmail.com', password = '12345678', userType = '1')
+        self.user1 = UserLogin(email = 'jim.scott@gmail.com', password = '12345678', userType = 1)
         db.session.add(self.user1)
         db.session.commit()
 
@@ -131,33 +134,30 @@ class TestCase(unittest.TestCase):
     
 
     # #testing that if a user books an event, it shows up on their my bookings page
-    # def test_bookedActivityShowsUp(self):
-    #     self.user2 = UserLogin(email = 'jim.halpert@gmail.com', password = '12345678', userType = '1')
-    #     db.session.add(self.user2)
-    #     db.session.commit()
+    def test_bookedActivityShowsUp(self):
+        self.user2 = UserLogin(email = 'jim.halpert@gmail.com', password = '12345678', userType = 1)
+        db.session.add(self.user2)
+        db.session.commit()
 
-    #     with self.app:
-    #         self.app.post('/login', data=dict(
-    #             email = self.user2.email,
-    #             password = self.user2.password
-    #         ))
+        with self.app:
+            self.app.post('/login', data=dict(
+                email = self.user2.email,
+                password = self.user2.password,
+                userType = self.user2.userType
+            ))
 
-    #         #create a new activity 
-    #         self.activity1 = Activity(
-    #             activityType = 'Test Activity', 
-    #             activityPrice = '15.00',
-    #             activityLocation = 'Test loc',
-    #             activityCapacity = '30',
-    #             activityStaffName = 'aaditi'
-    #         )
-    #         db.session.add(self.activity1)
-    #         db.session.commit()
+            #create a new activity 
+            self.activity1 = Activity(activityType = 'Test Activity')
+            db.session.add(self.activity1)
+            db.session.commit()
 
-    #         #book the activity 
-    #         self.app.post(f'/calendar/{self.activity1.id}/makeBooking')
+            #make a calendar event for the activity 
 
-    #         #check if activity shows up on the user's my bookings page 
-    #         response = self.app.get('/myBookings')
-    #         self.assertIn(self.activity1.activityType.encode(), response.data)
+            #book the activity 
+            self.app.post(f'/calendar/{self.activity1.id}/makeBooking')
+
+            #check if activity shows up on the user's my bookings page 
+            response = self.app.get('/myBookings')
+            self.assertIn(self.activity1.activityType.encode(), response.data)
 
     #testing that is a user cancels their booking, it is removed from their my bookings page 
